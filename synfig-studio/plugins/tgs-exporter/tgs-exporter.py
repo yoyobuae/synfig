a5515017b751924d30c728eb07d9f29c8bc3e870
Gerald Young <gerald.young@azetti.com>
2019-07-09 16:47:28 -0500

    Added tgs-export plugin which references lottie-exporter

---
+++ synfig-studio/plugins/tgs-exporter/tgs-exporter.py
@@ -0,0 +1,32 @@
+# pylint: disable=line-too-long
+"""
+Python plugin to convert the .sif format into TGS Telegram format
+input   : FILE_NAME.sif
+output  : FILE_NAME.tgs
+"""
+import os
+import sys
+import gzip
+import shutil
+from pathlib import Path
+
+sys.path.insert(1, os.path.join(sys.path[0], '..', 'lottie-exporter'))
+
+import settings
+_temp = __import__('lottie-exporter', globals(), locals(), ['parse'], 0)
+parse = _temp.parse
+
+
+def GenerateSticker(file_name):
+    with open(file_name, 'rb') as f_in:
+        with gzip.open((file_name.rsplit(".")[0])+".tgs", 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+
+if len(sys.argv) < 2:
+    sys.exit()
+else:
+    settings.init()
+    FILE_NAME = sys.argv[1]
+    new_file_name = parse(FILE_NAME)
+    GenerateSticker(new_file_name)
